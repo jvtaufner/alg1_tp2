91df35029e53164ac85e6f581b35d4904328370e
taufner <jvtaufner@hotmail.com>
2021-07-24 17:55:42 -0300

    consertado problema do print e do add

--- src/graph.cpp
+++ src/graph.cpp
@@ -9,12 +9,13 @@ Graph::Graph(int numberOfNodes, int numberOfEdges){
 }
 
 void Graph::addEdge(int node1, int node2){
-    adjList[node1].push_back(node2);
+    //número dos nós começa em 1
+    adjList[node1-1].push_back(node2);
 }
 
 void Graph::printGraph(){
     for(int i=0; i<numberOfNodes; i++){
-        cout << "Lista de " << i << endl;
+        cout << "Lista de " << i+1 << endl;
         for(auto v : adjList[i]){
             cout << v << endl;
         }
